eb425a20233c8327884f870e4288d4a3b3b5cc94
joel-cM <joelchavezm19@gmail.com>
2022-09-22 17:20:10 -0300

    react-cookie installed and CookieProvider added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import "../styles/global.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "../components/Layout";
+import { CookiesProvider } from "react-cookie";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
+    <CookiesProvider>
       <Component {...pageProps} />
-    </Layout>
+    </CookiesProvider>
   );
 }
