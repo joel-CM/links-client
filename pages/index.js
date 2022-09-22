12681b27d7416528f9fd4a245aa063705fd7b338
joel-cM <joelchavezm19@gmail.com>
2022-09-22 17:21:20 -0300

    varify token added on pageIndex

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
 import Home from "../components/Home";
+import verifyToken from "../helpers/verifyToken";
 
-export default function Index() {
+export default function PageIndex() {
   return <Home />;
+}
+
+export async function getServerSideProps(ctx) {
+  const token = ctx.req.cookies.token;
+
+  if (!token) {
+    return {
+      props: {},
+    };
+  }
+
+  const verify = await verifyToken(token);
+  if (verify.error) {
+    return {
+      props: {},
+    };
+  }
+
+  return {
+    redirect: {
+      destination: "/app",
+      permanent: false,
+    },
+  };
 }
